cd63110a3c22febd639c9915dc01afe4c77d91f6
Omar Siam <Omar.Siam@oeaw.ac.at>
2013-10-30 18:38:35 +0100

    Refactor common parts into common.php. Refactor BibliographyOnSRU.php to work like ProfileOnSRU.php but for the bibliography database. Start of refactoring for SampleTextOnSRU.php

--- BibliographyOnSRU.php
+++ BibliographyOnSRU.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * This file script provides SRU access to a mysql database containing TEI data
+ * This script provides SRU access to a mysql database containing TEI data
  * language profiles.
- * 
+ * This script is responsible for handling FCS requests for bibliographic data. 
  * 
  * @uses $dbConfigfile
  * @uses $operation
@@ -14,216 +14,126 @@
  * @package mysqlonsru
  */
 
-error_reporting(E_ALL);
+/**
+ * Load configuration and common functions
+ */
 
- //load configuration
- include "../utils-php/config.php";
+require_once "common.php";
 
- //load database and user data
- include $dbConfigFile;
-
- //TODO: actually create that $responseTemplateFcs file ...
- if (isset($_GET['x-type']) && trim($_GET['x-type']) == "fcs")
-   $responseTemplate = $responseTemplateFcs;
-
- function diagnostics($dgId, $dgDetails)
- {
-    global $diagnosticsTemplate;
-    global $version;
-    global $vlibPath;
-
-    //TODO: this needs to be based on diagnostics-list:
-    //http://www.loc.gov/standards/sru/resources/diagnostics-list.html
-    $diagnosticMessage = "Error";
-    $diagnosticId = $dgId;
-    $diagnosticDetails = $dgDetails;
-
-    require_once $vlibPath;
-
-    $tmpl = new vlibTemplate($diagnosticsTemplate);
-
-    $tmpl->setvar('version', $version);
-    $tmpl->setvar('diagnosticId', $diagnosticId);
-    $tmpl->setvar('diagnosticMessage', $diagnosticMessage);
-    $tmpl->setvar('diagnosticDetails', $diagnosticDetails);
-
-    $tmpl->pparse();
- }
 
 /**
- * Return the explain template with the host id filled in
+ * Generates a response according to ZeeRex
+ * 
+ * This is a machine readable description of this script's capabilities.
+ * 
+ * @see http://zeerex.z3950.org/overview/index.html
  * 
  * @uses $explainTemplate
- * @uses $localhost
- * @uses $vlibPath 
  */
  function explain()
  {
     global $explainTemplate;
-    global $vlibPath;
-    global $localhost;
-
-    require_once $vlibPath;
 
     $tmpl = new vlibTemplate($explainTemplate);
-    $tmpl->setVar('hostid', $localhost);
+    
+    $maps = array();
+    
+    array_push($maps, array(
+         'title' => 'VICAV Bibliography',
+        'name' => 'bib',
+        'search' => 'true',
+        'scan' => 'true',
+        'sort' => 'false',
+    ));
+    
+    array_push($maps, array(
+        'title' => 'VICAV Bibliography by VICAV Taxonomy',
+        'name' => 'vicavTaxonomy',
+        'search' => 'true',
+        'scan' => 'true',
+        'sort' => 'false',
+    ));
+    
+    $tmpl->setLoop('maps', $maps);
+    
+    $tmpl->setVar('hostid', htmlentities($_SERVER["HTTP_HOST"]));
+    $tmpl->setVar('database', 'vicav-bib');
+    $tmpl->setVar('databaseTitle', 'VICAV Bibliography');
     $tmpl->pparse();
- }
-
- function decodecharrefs($str)
- {
-   //str_replace("alt","neu","Zeichenkette")
-   $str = str_replace("#9#", ";", $str);
-   $str = str_replace("#8#", "&#", $str);
-   $str = str_replace("&#amp;", "&amp;", $str);
-   return $str;
  }
  
  /**
-  * Function implementing searchRetrieve
+  * Searches vicav_profiles_001 database using the lemma column
   * 
-  * @uses $responseTemplate
-  * @uses $responseTemplateFcs
-  * @uses $version
-  * @uses $recordSchema
-  * @uses $recordPacking
-  * @uses $server
-  * @uses $user
-  * @uses $password
-  * @uses $database
-  * @uses $vlibPath
+  * @uses $sru_fcs_params
   */
- function search($query)
+ function search()
  {
-    global $responseTemplate;
-    global $version;
-    global $recordSchema;
-    global $recordPacking;
+    global $sru_fcs_params;
 
-    global $server;
-    global $user;
-    global $password;
-    global $database;
-
-    global $vlibPath;
-
-    require_once $vlibPath;
-
-    $queryParts = explode("=", $query);
-    //print_r($queryParts);
-
-    if (count($queryParts) < 2)
-    {
-      unset($queryParts);
-      $queryParts[0] = "vicavTaxonomy";
-      $queryParts[1] = $query;
+    $db = db_connect();
+    if ($db->connect_errno) {
+        return;
     }
-
-    if ($queryParts[0] == "vicavTaxonomy")
-      $xPath = "biblStruct-note-index-term-vicavTaxonomy-";
-    else if ($queryParts[0] == "id")
-      $xPath = "biblStruct-xml:id";
-
-    $db = mysql_connect($server, $user, $password);
-    if (!$db)
-      diagnostics('MySQl Connection Error', 'Failed to connect to database: ' . mysql_error());
-    mysql_select_db($database, $db);
-
-    $sqlstr = "SELECT DISTINCT b.id, b.entry FROM vicav_bibl_002 b, vicav_bibl_002_ndx ndx ";
-    $sqlstr.= "WHERE ndx.xpath='$xPath' AND ndx.txt='$queryParts[1]' AND ndx.id=b.id ";
-
-    //print "sqlstr: '$sqlstr'";
-
-    $result = mysql_query($sqlstr);
-
-    $numberOfRecords = mysql_num_rows($result);
-
-    $tmpl = new vlibTemplate($responseTemplate);
-
-    $tmpl->setvar('version', $version);
-    $tmpl->setvar('numberOfRecords', $numberOfRecords);
-    $tmpl->setvar('query', $query);
-    $tmpl->setvar('baseURL', $baseURL);
-    $tmpl->setvar('nextRecordPosition', $nextRecordPosition);
-    $tmpl->setvar('res', '1');
-
-    $hits = array();
-    $hitsMetaData = array();
-    array_push($hitsMetaData, array('key' => 'copyright', 'value' => 'ICLTT'));
-    $hstr = "Bibliography for the region of $query";
-    array_push($hitsMetaData, array('key' => 'content', 'value' => $hstr));
-
-    while ($line = mysql_fetch_row($result))
-    {
-        $id = $line[0];
-        $content = $line[1];
-
-        array_push($hits, array(
-               'recordSchema' => $recordSchema,
-               'recordPacking' => $recordPacking,
-               'queryUrl' => $baseURL,
-               'content' => decodecharrefs($content),
-               'hitsMetaData' => $hitsMetaData
-               ));
-    }
-
-    $tmpl->setloop('hits', $hits);
-    $tmpl->pparse();
- }
-
-  //sru params
-  if (isset($_GET['operation']) && trim($_GET['operation']) != "")
-    $operation = trim($_GET['operation']);
-  else
-    $operation = "";
-
-  if (isset($_GET['version']) && trim($_GET['version']) != "")
-    $version = trim($_GET['version']);
-
-  if (isset($_GET['startRecord']) && trim($_GET['startRecord']) != "")
-    $startRecord = trim($_GET['startRecord']);
-  else
-    $startRecord = "";
-
-  if (isset($_GET['maximumRecords']) && trim($_GET['maximumRecords']) != "")
-    $maximumRecords = trim($_GET['maximumRecords']);
-  else
-    $maximumRecords = "";
-
-  if (isset($_GET['query']) && trim($_GET['query']) != "")
-    $query = trim($_GET['query']);
-  else
+    
+    // HACK, sql parser? cql.php = GPL -> this GPL too
+    $sru_fcs_params->query = str_replace("\"", "", $sru_fcs_params->query);
     $query = "";
+    $profile_query = preg_filter('/id *(=|any) *(.*)/', '$2', $sru_fcs_params->query);
+    if (!isset($profile_query)) {
+        $profile_query = preg_filter('/(cql\.)?serverChoice *(=|any) *(.*)/', '$3', $sru_fcs_params->query);
+    }
+    $vicavTaxonomy_query = preg_filter('/vicavTaxonomy *(=|any) *(.*)/', '$2', $sru_fcs_params->query);
+    
+    if (isset($vicavTaxonomy_query)){
+        $query = $db->escape_string($vicavTaxonomy_query);
+        $sqlstr = sqlForXPath("vicav_bibl_002", "-index-term-vicavTaxonomy-", array("query" => $query));
+    } else {
+       if (isset($profile_query)) {
+           $query = $db->escape_string($profile_query);
+       } else {
+           $query = $db->escape_string($sru_fcs_params->query);
+       }
+       $sqlstr = sqlForXPath("vicav_bibl_002", "biblStruct-xml:id", array("query" => $query));
+    }
 
-  if (isset($_GET['scanClause']) && trim($_GET['scanClause']) != "")
-    $scanClause = trim($_GET['scanClause']);
-  else
-    $scanClause = "";
+    populateSearchResult($db, $sqlstr, "Bibliography for the region of $query");
+}
 
-  if (isset($_GET['recordPacking']) && trim($_GET['recordPacking']) == "xml")
-    $recordPacking = trim($_GET['recordPacking']);
-  else
-    $recordPacking = "raw";
+ /**
+ * Lists the entries from the lemma column in the vicav_profiles_001 database
+ * 
+ * Lists either the profiles (city names) or the sample texts ([id])
+ * 
+ * @see http://www.loc.gov/standards/sru/specs/scan.html
+ * 
+ * @uses $sru_fcs_params
+ */
+function scan() {
+    global $sru_fcs_params;
 
-  header ("content-type: text/xml");
-  //print "test";
+    $db = db_connect();
+    if ($db->connect_errno) {
+        return;
+    }
+    
+    $sqlstr = '';
+    
+    if ($sru_fcs_params->scanClause === '' ||
+        $sru_fcs_params->scanClause === 'id' ||
+        $sru_fcs_params->scanClause === 'serverChoice' ||
+        $sru_fcs_params->scanClause === 'cql.serverChoice') {
+       $sqlstr = sqlForXPath("vicav_bibl_002", "biblStruct-xml:id", array("filter" => "-"));     
+    } else if ($sru_fcs_params->scanClause === 'vicavTaxonomy') {
+       $sqlstr = sqlForXPath("vicav_bibl_002", "-index-term-vicavTaxonomy-", array("filter" => "-")); 
+    } else {
+        diagnostics(51, 'Result set: ' . $sru_fcs_params->scanClause);
+        return;
+    }
+    
+    populateScanResult($db, $sqlstr);
+}
 
-  if ($operation == "explain" || $operation == "")
-    explain();
-  else if ($operation == "scan")
-  {
-
-  }
-  else if ($operation == "searchRetrieve")
-  {
-    //print "query: $query";
-    search($query);
-  }
-  else
-    diagnostics("operation unknown", "The operation '$operation' is not SRU compliant");
-
-
-    //diagnostics('test1', 'test message');
-
-?>
+getParamsAndSetUpHeader();
+processRequest();
 
--- SampleTextOnSRU.php
+++ SampleTextOnSRU.php
@@ -1,176 +1,138 @@
 <?php
+/**
+ * This script provides SRU access to a mysql database containing TEI data
+ * language profiles.
+ * This script is responsible for handling FCS requests for language profile data. 
+ * 
+ * @uses $dbConfigfile
+ * @uses $sru_fcs_params
+ * @uses $responseTemplate
+ * @uses $responseTemplateFcs
+ * @package mysqlonsru
+ */
+//error_reporting(E_ALL);
 
-error_reporting(E_ALL);
+/**
+ * Load configuration and common functions
+ */
+require_once "common.php";
 
- //load configuration
-include "../utils-php/config.php";
-
- //load database and user data
- include $dbConfigFile;
-
- if (isset($_GET['x-type']) && trim($_GET['x-type']) == "fcs")
-   $responseTemplate = $responseTemplateFcs;
-
-
- function diagnostics($dgId, $dgDetails)
- {
-    global $diagnosticsTemplate;
-    global $version;
-    global $vlibPath;
-
-    //TODO: this needs to be based on diagnostics-list:
-    //http://www.loc.gov/standards/sru/resources/diagnostics-list.html
-    $diagnosticMessage = "Error";
-    $diagnosticId = $dgId;
-    $diagnosticDetails = $dgDetails;
-
-    require_once $vlibPath;
-
-   	$tmpl = new vlibTemplate($diagnosticsTemplate);
-
-   	$tmpl->setvar('version', $version);
-   	$tmpl->setvar('diagnosticId', $diagnosticId);
-   	$tmpl->setvar('diagnosticMessage', $diagnosticMessage);
-   	$tmpl->setvar('diagnosticDetails', $diagnosticDetails);
-
-   	$tmpl->pparse();
- }
-
+/**
+ * Generates a response according to ZeeRex
+ * 
+ * This is a machine readable description of this script's capabilities.
+ * 
+ * @see http://zeerex.z3950.org/overview/index.html
+ * 
+ * @uses $explainTemplate
+ */
  function explain()
  {
     global $explainTemplate;
-    global $vlibPath;
 
-    require_once $vlibPath;
-
-   	$tmpl = new vlibTemplate($explainTemplate);
-   	$tmpl->pparse();
+    $tmpl = new vlibTemplate($explainTemplate);
+    
+    $maps = array();
+    
+    array_push($maps, array(
+        'title' => 'VICAV Profile Sample Text',
+        'name' => 'sampleText',
+        'search' => 'true',
+        'scan' => 'true',
+        'sort' => 'false',
+    ));
+    
+    array_push($maps, array(
+        'title' => 'VICAV Sample Text for Geo Coordinates',
+        'name' => 'geo',
+        'search' => 'true',
+        'scan' => 'true',
+        'sort' => 'false',
+    ));
+    
+    $tmpl->setLoop('maps', $maps);
+    
+    $tmpl->setVar('hostid', htmlentities($_SERVER["HTTP_HOST"]));
+    $tmpl->setVar('database', 'vicav-text');
+    $tmpl->setVar('databaseTitle', 'VICAV Profile');
+    $tmpl->pparse();
  }
 
- function decodecharrefs($str)
- {
-   //str_replace("alt","neu","Zeichenkette")
-   $str = str_replace("#9#", ";", $str);
-   $str = str_replace("#8#", "&#", $str);
-   return $str;
- }
+ /**
+  * Searches vicav_profiles_001 database using the lemma column
+  * 
+  * @uses $responseTemplate
+  * @uses $sru_fcs_params
+  * @uses $baseURL
+  */
+ function search() {
+    global $sru_fcs_params;
 
- function search($query)
- {
-    global $responseTemplate;
-    global $version;
-    global $recordSchema;
-    global $recordPacking;
-
-    global $server;
-    global $user;
-    global $password;
-    global $database;
-
-    global $vlibPath;
-
-    require_once $vlibPath;
-
-    $db = mysql_connect($server, $user, $password);
-    if (!$db)
-      diagnostics('MySQl Connection Error', 'Failed to connect to database: ' . mysql_error());
-    mysql_select_db($database, $db);
-
-    $sqlstr = "SELECT DISTINCT id, entry FROM vicav_profiles_001 ";
-    $sqlstr.= "WHERE sid = '$query'";
-
-    //print "sqlstr: '$sqlstr'";
-
-    $result = mysql_query($sqlstr);
-
-    $numberOfRecords = mysql_num_rows($result);
-
-   	$tmpl = new vlibTemplate($responseTemplate);
-
-   	$tmpl->setvar('version', $version);
-   	$tmpl->setvar('numberOfRecords', $numberOfRecords);
-   	$tmpl->setvar('query', $query);
-   	$tmpl->setvar('baseURL', $baseURL);
-   	$tmpl->setvar('nextRecordPosition', $nextRecordPosition);
-   	$tmpl->setvar('res', '1');
-
-   	$hits = array();
-   	$hitsMetaData = array();
-   	array_push($hitsMetaData, array('key' => 'copyright', 'value' => 'ICLTT'));
-   	$hstr = "Arabic dialect sample text for the region of $query";
-   	array_push($hitsMetaData, array('key' => 'content', 'value' => $hstr));
-
-    if ($line = mysql_fetch_row($result))
-    {
-        $id = $line[0];
-        $content = $line[1];
-
-        array_push($hits, array(
-               'recordSchema' => $recordSchema,
-               'recordPacking' => $recordPacking,
-               'queryUrl' => $baseURL,
-               'content' => decodecharrefs($content),
-               'hitsMetaData' => $hitsMetaData
-               ));
+    $db = db_connect();
+    if ($db->connect_errno) {
+        return;
     }
-
-    $tmpl->setloop('hits', $hits);
-   	$tmpl->pparse();
- }
-
-  //sru params
-  if (isset($_GET['operation']) && trim($_GET['operation']) != "")
-    $operation = trim($_GET['operation']);
-  else
-    $operation = "";
-
-  if (isset($_GET['version']) && trim($_GET['version']) != "")
-    $version = trim($_GET['version']);
-
-  if (isset($_GET['startRecord']) && trim($_GET['startRecord']) != "")
-    $startRecord = trim($_GET['startRecord']);
-  else
-    $startRecord = "";
-
-  if (isset($_GET['maximumRecords']) && trim($_GET['maximumRecords']) != "")
-    $maximumRecords = trim($_GET['maximumRecords']);
-  else
-    $maximumRecords = "";
-
-  if (isset($_GET['query']) && trim($_GET['query']) != "")
-    $query = trim($_GET['query']);
-  else
+    // HACK, sql parser? cql.php = GPL -> this GPL too
+    $sru_fcs_params->query = str_replace("\"", "", $sru_fcs_params->query);
     $query = "";
+    $description = "";
+    $sampleText_query = preg_filter('/sampleText *(=|any) *(.*)/', '$2', $sru_fcs_params->query);
+    if (!isset($sampleText_query)) {
+        $sampleText_query = preg_filter('/(cql\.)?serverChoice *(=|any) *(.*)/', '$3', $sru_fcs_params->query);
+    }
+    $geo_query = preg_filter('/geo *(=|any) *(.*)/', '$2', $sru_fcs_params->query);
+    if (isset($geo_query)){
+        $query = $db->escape_string($geo_query);
+        $description = "Arabic sample text for the coordinates $query";
+        $sqlstr = sqlForXPath("vicav_profiles_001", "geo-", array("query" => $query));
+    } else {
+       if (isset($sampleText_query)) {
+           $query = $db->escape_string($sampleText_query);
+       } else {
+           $query = $db->escape_string($sru_fcs_params->query);
+       }
+       $description = "Arabic dialect sample text for the region of $query";
+        $sqlstr = "SELECT DISTINCT sid, entry FROM vicav_profiles_001 ";
+        $sqlstr.= "WHERE sid = '$query'";
+    }
+    populateSearchResult($db, $sqlstr, $description);
+}
 
-  if (isset($_GET['scanClause']) && trim($_GET['scanClause']) != "")
-    $scanClause = trim($_GET['scanClause']);
-  else
-    $scanClause = "";
+/**
+ * Lists the entries from the lemma column in the vicav_profiles_001 database
+ * 
+ * Lists either the profiles (city names) or the sample texts ([id])
+ * 
+ * @see http://www.loc.gov/standards/sru/specs/scan.html
+ * 
+ * @uses $scanTemplate
+ * @uses $sru_fcs_params
+ */
+function scan() {
+    global $sru_fcs_params;
 
-  if (isset($_GET['recordPacking']) && trim($_GET['recordPacking']) == "xml")
-    $recordPacking = trim($_GET['recordPacking']);
-  else
-    $recordPacking = "raw";
+    $db = db_connect();
+    if ($db->connect_errno) {
+        return;
+    }
+    
+    $sqlstr = '';
+    
+    if ($sru_fcs_params->scanClause === '' ||
+        $sru_fcs_params->scanClause === 'sampleText' ||
+        $sru_fcs_params->scanClause === 'serverChoice' ||
+        $sru_fcs_params->scanClause === 'cql.serverChoice') {
+       $sqlstr = "SELECT DISTINCT sid, id FROM vicav_profiles_001 " .
+              "WHERE sid LIKE '%_sample_%'"; 
+    } else if ($sru_fcs_params->scanClause === 'geo') {
+       $sqlstr = sqlForXPath("vicav_profiles_001", "geo-"); 
+    } else {
+        diagnostics(51, 'Result set: ' . $sru_fcs_params->scanClause);
+        return;
+    }
+    
+    populateScanResult($db, $sqlstr);
+}
 
-  header ("content-type: text/xml");
-  //print "test";
-
-  if ($operation == "explain" || $operation == "")
-    explain();
-  else if ($operation == "scan")
-  {
-
-  }
-  else if ($operation == "searchRetrieve")
-  {
-    //print "query: $query";
-    search($query);
-  }
-  else
-    diagnostics("operation unknown", "The operation '$operation' is not SRU compliant");
-
-
-    //diagnostics('test1', 'test message');
-
-?>
-
+getParamsAndSetUpHeader();
+processRequest();
