ace6048527a5c655f71053b1c5e3d1a261022af9
Omar Siam <Omar.Siam@oeaw.ac.at>
2014-02-12 17:30:48 +0100

    Enable ProfileOnSRU to handle sampletexts in another dbtable.

--- ProfileOnSRU.php
+++ ProfileOnSRU.php
@@ -36,41 +36,44 @@ require_once "common.php";
     
     $maps = array();
     
-    array_push($maps, array(
-        'title' => 'VICAV Profile',
-        'name' => 'profile',
-        'search' => 'true',
-        'scan' => 'true',
-        'sort' => 'false',
-    ));
-    
-    array_push($maps, array(
-        'title' => 'VICAV Profile Sample Text',
-        'name' => 'sampleText',
-        'search' => 'true',
-        'scan' => 'true',
-        'sort' => 'false',
-    ));
-    
-    array_push($maps, array(
-        'title' => 'VICAV Profile Geo Coordinates',
-        'name' => 'geo',
-        'search' => 'true',
-        'scan' => 'true',
-        'sort' => 'false',
-    ));
-    
-    populateExplainResult($db, "vicav_profiles_001", "vicav-profile", $maps);
+    global $profileTable;
+    if (stripos($profileTable, "profile") !== false) {
+        array_push($maps, array(
+            'title' => 'VICAV Profile',
+            'name' => 'profile',
+            'search' => 'true',
+            'scan' => 'true',
+            'sort' => 'false',
+        ));
+
+        array_push($maps, array(
+            'title' => 'VICAV Profile Geo Coordinates',
+            'name' => 'geo',
+            'search' => 'true',
+            'scan' => 'true',
+            'sort' => 'false',
+        ));
+    } else if (stripos($profileTable, "sampletext") !== false) {
+        array_push($maps, array(
+            'title' => 'VICAV Profile Sample Text',
+            'name' => 'sampleText',
+            'search' => 'true',
+            'scan' => 'true',
+            'sort' => 'false',
+        ));
+    }
+    populateExplainResult($db, "$profileTable", "$profileTable", $maps);
  }
 
  /**
-  * Searches vicav_profiles_001 database using the lemma column
+  * Searches $profileTable database using the lemma column
   * 
   * @uses $responseTemplate
   * @uses $sru_fcs_params
   * @uses $baseURL
   */
  function search() {
+    global $profileTable;
     global $sru_fcs_params;
 
     $db = db_connect();
@@ -82,36 +85,40 @@ require_once "common.php";
     $query = "";
     $description = "";
     $profile_query = get_search_term_for_wildcard_search("profile", $sru_fcs_params->query);
-    if (!isset($profile_query)) {
+    if (!isset($profile_query) && (stripos($profileTable, "profile") !== false)) {
         $profile_query = get_search_term_for_wildcard_search("serverChoice", $sru_fcs_params->query, "cql");
     }
     $sampleText_query = get_search_term_for_wildcard_search("sampleText", $sru_fcs_params->query);
+    if (!isset($profile_query) && (stripos($profileTable, "sampletext") !== false)) {
+        $sampleText_query = get_search_term_for_wildcard_search("serverChoice", $sru_fcs_params->query, "cql");
+    }
     $geo_query = get_search_term_for_wildcard_search("geo", $sru_fcs_params->query);
     $profile_query_exact = get_search_term_for_exact_search("profile", $sru_fcs_params->query);
-    if (!isset($profile_query)) {
+    if (!isset($profile_query_exact) && (stripos($profileTable, "profile") !== false)) {
         $profile_query_exact = get_search_term_for_exact_search("serverChoice", $sru_fcs_params->query, "cql");
     }
     $sampleText_query_exact = get_search_term_for_exact_search("sampleText", $sru_fcs_params->query);
+    if (!isset($profile_query_exact) && (stripos($profileTable, "sampletext") !== false)) {
+        $sampleText_query_exact = get_search_term_for_exact_search("serverChoice", $sru_fcs_params->query, "cql");
+    }
     $geo_query_exact = get_search_term_for_exact_search("geo", $sru_fcs_params->query);
     // there is no point in having a fuzzy geo search yet so treat it as exact always
     if (!isset($geo_query_exact)) {
         $geo_query_exact = $geo_query;
     }
     $options = array (
-       "dbtable" => "vicav_profiles_001",
+       "dbtable" => "$profileTable",
        "query" => $query,
        "distinct-values" => false,
     );
     if (isset($sampleText_query_exact)) {
-        $query = $db->escape_string($sampleText_query_exact);
-        $description = "Arabic dialect sample text for the region of $query";
-        $sqlstr = "SELECT DISTINCT sid, entry FROM vicav_profiles_001 ";
-        $sqlstr.= "WHERE sid = '$query'";
+        $regionGuess = explode('_', $sampleText_query_exact);
+        populateSampleTextResult($db->escape_string($sampleText_query_exact), $db, $regionGuess[0]);
+        return;
     } else if (isset($sampleText_query)) {
-        $query = $db->escape_string($sampleText_query);
-        $description = "Arabic dialect sample text for the region of $query";
-        $sqlstr = "SELECT DISTINCT sid, entry FROM vicav_profiles_001 ";
-        $sqlstr.= "WHERE sid LIKE '%" . $query . "_sample%'";
+        $regionGuess = explode('_', $sampleText_query);
+        populateSampleTextResult("%" . $db->escape_string($sampleText_query) . "_sample%", $db, $regionGuess[0]);
+        return;
     } else if (isset($geo_query_exact)){
         $query = $db->escape_string($geo_query_exact);
         $description = "Arabic dialect profile for the coordinates $geo_query_exact";
@@ -128,18 +135,34 @@ require_once "common.php";
            $query = $db->escape_string($sru_fcs_params->query);
        }
        $description = "Arabic dialect profile for the region of $query"; 
-       $sqlstr = "SELECT DISTINCT id, entry FROM vicav_profiles_001 ";
+       $sqlstr = "SELECT DISTINCT id, entry FROM $profileTable ";
        if (isset($profile_query_exact)) {
           $sqlstr.= "WHERE lemma = '" . encodecharrefs($query) . "'"; 
        } else {
-          $sqlstr.= "WHERE lemma LIKE '%" . encodecharrefs($query) . "%'";
-       }
+           if (stripos($profileTable, "profile") !== false) {
+                $sqlstr.= "WHERE lemma LIKE '%" . encodecharrefs($query) . "%'";
+           } else if (stripos($profileTable, "sampletext") !== false) {
+                $regionGuess = explode('_', $query);
+                populateSampleTextResult("%" . $db->escape_string(encodecharrefs($query)) . "_sample%", $db, $regionGuess[0]);
+                return;
+            }
+        }
     }   
     populateSearchResult($db, $sqlstr, $description);
 }
 
+function populateSampleTextResult($escapedQueryString, $db, $region) {
+    global $profileTable;
+
+    $description = "Arabic dialect sample text for the region of $region";
+    $sqlstr = "SELECT DISTINCT sid, entry FROM $profileTable ";
+    $sqlstr.= "WHERE sid " . 
+            (strpos($escapedQueryString, '%') !== false ? 'LIKE' : '=') .
+            " '$escapedQueryString'";
+    populateSearchResult($db, $sqlstr, $description);
+}
 /**
- * Lists the entries from the lemma column in the vicav_profiles_001 database
+ * Lists the entries from the lemma column in the $profileTable database
  * 
  * Lists either the profiles (city names) or the sample texts ([id])
  * 
@@ -150,6 +173,7 @@ require_once "common.php";
  */
 function scan() {
     global $sru_fcs_params;
+    global $profileTable;
 
     $db = db_connect();
     if ($db->connect_errno) {
@@ -158,17 +182,22 @@ function scan() {
     
     $sqlstr = '';
     
-    if ($sru_fcs_params->scanClause === '' ||
-        $sru_fcs_params->scanClause === 'profile' ||
-        $sru_fcs_params->scanClause === 'serverChoice' ||
-        $sru_fcs_params->scanClause === 'cql.serverChoice') {
-       $sqlstr = "SELECT DISTINCT lemma, id, sid, COUNT(*) FROM vicav_profiles_001 " .
+    if ($sru_fcs_params->scanClause === 'profile' ||
+        ((stripos($profileTable, "profile") !== false) &&
+        ($sru_fcs_params->scanClause === '' ||
+         $sru_fcs_params->scanClause === 'serverChoice' ||
+         $sru_fcs_params->scanClause === 'cql.serverChoice'))) {
+       $sqlstr = "SELECT DISTINCT lemma, id, sid, COUNT(*) FROM $profileTable " .
               "WHERE lemma NOT LIKE '[%]' GROUP BY lemma";   
-    } else if ($sru_fcs_params->scanClause === 'sampleText') {
-       $sqlstr = "SELECT DISTINCT sid, id, sid, COUNT(*) FROM vicav_profiles_001 " .
+    } else if ($sru_fcs_params->scanClause === 'sampleText' ||
+        ((stripos($profileTable, "sampletext") !== false) &&
+        ($sru_fcs_params->scanClause === '' ||
+         $sru_fcs_params->scanClause === 'serverChoice' ||
+         $sru_fcs_params->scanClause === 'cql.serverChoice'))) {
+       $sqlstr = "SELECT DISTINCT sid, id, sid, COUNT(*) FROM $profileTable " .
               "WHERE sid LIKE '%_sample_%' GROUP BY sid";           
     } else if ($sru_fcs_params->scanClause === 'geo') {
-       $sqlstr = sqlForXPath("vicav_profiles_001", "geo-",
+       $sqlstr = sqlForXPath("$profileTable", "geo-",
                array("show-lemma" => true,
                      "distinct-values" => true,
            )); 
@@ -181,5 +210,6 @@ function scan() {
 }
 
 \ACDH\FCSSRU\getParamsAndSetUpHeader();
+$profileTable = $sru_fcs_params->xcontext;
 processRequest();
 
